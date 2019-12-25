a742dfe49dbcf121f599f3d8c23968d5bc2a753d
tanchaoju <1120153473@qq.com>
2019-12-26 00:48:07 +0800

    2.封装axios

--- headnews/src/main.js
+++ headnews/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/styles/reset.css'
+import { Toast } from 'vant'
+
 Vue.config.productionTip = false
 
+Vue.use(Toast)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
